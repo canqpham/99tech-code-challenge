f32cef8b8d28722a6cb42c3c9d1ec7834190871f
cang-pham-codeleap <cang.pham@codeleap.de>
2025-10-02 20:35:18 +0700

    chore: update Node.js to v22 and configure Vite base URL for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/99tech-code-challenge/",
   plugins: [
     react({
       babel: {
